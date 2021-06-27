8b42d5bbf5ce39220620dd0c40defcc251021c71
kongjonghun <rhd0503@naver.com>
2021-06-27 18:33:23 +0900

    dijkstra

---
+++ Final/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.rbTo = new System.Windows.Forms.RadioButton();
+            this.rbFrom = new System.Windows.Forms.RadioButton();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbTravelTime = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbRouteLength = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbSearch = new System.Windows.Forms.ListBox();
+            this.btnNavi = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.axMap1 = new AxMapWinGIS.AxMap();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnRefresh);
+            this.splitContainer1.Panel1.Controls.Add(this.rbTo);
+            this.splitContainer1.Panel1.Controls.Add(this.rbFrom);
+            this.splitContainer1.Panel1.Controls.Add(this.tbTo);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.tbFrom);
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.tbTravelTime);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.tbRouteLength);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.lbSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.btnNavi);
+            this.splitContainer1.Panel1.Controls.Add(this.btnSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.tbSearch);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(903, 623);
+            this.splitContainer1.SplitterDistance = 182;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(96, 583);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(77, 23);
+            this.btnRefresh.TabIndex = 19;
+            this.btnRefresh.Text = "초기화";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // rbTo
+            // 
+            this.rbTo.AutoSize = true;
+            this.rbTo.Location = new System.Drawing.Point(96, 11);
+            this.rbTo.Name = "rbTo";
+            this.rbTo.Size = new System.Drawing.Size(59, 16);
+            this.rbTo.TabIndex = 18;
+            this.rbTo.TabStop = true;
+            this.rbTo.Text = "도착지";
+            this.rbTo.UseVisualStyleBackColor = true;
+            // 
+            // rbFrom
+            // 
+            this.rbFrom.AutoSize = true;
+            this.rbFrom.Location = new System.Drawing.Point(30, 11);
+            this.rbFrom.Name = "rbFrom";
+            this.rbFrom.Size = new System.Drawing.Size(59, 16);
+            this.rbFrom.TabIndex = 17;
+            this.rbFrom.TabStop = true;
+            this.rbFrom.Text = "출발지";
+            this.rbFrom.UseVisualStyleBackColor = true;
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(63, 381);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(110, 21);
+            this.tbTo.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 386);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 12);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "도착지:";
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Location = new System.Drawing.Point(63, 354);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(110, 21);
+            this.tbFrom.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "출발지:";
+            // 
+            // tbTravelTime
+            // 
+            this.tbTravelTime.Location = new System.Drawing.Point(77, 535);
+            this.tbTravelTime.Name = "tbTravelTime";
+            this.tbTravelTime.Size = new System.Drawing.Size(96, 21);
+            this.tbTravelTime.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 517);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "예상 소요시간(분):";
+            // 
+            // tbRouteLength
+            // 
+            this.tbRouteLength.Location = new System.Drawing.Point(77, 476);
+            this.tbRouteLength.Name = "tbRouteLength";
+            this.tbRouteLength.Size = new System.Drawing.Size(96, 21);
+            this.tbRouteLength.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 455);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "총 길이(km):";
+            // 
+            // lbSearch
+            // 
+            this.lbSearch.FormattingEnabled = true;
+            this.lbSearch.ItemHeight = 12;
+            this.lbSearch.Location = new System.Drawing.Point(12, 63);
+            this.lbSearch.Name = "lbSearch";
+            this.lbSearch.Size = new System.Drawing.Size(161, 280);
+            this.lbSearch.TabIndex = 8;
+            this.lbSearch.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbSearch_MouseDoubleClick);
+            // 
+            // btnNavi
+            // 
+            this.btnNavi.Location = new System.Drawing.Point(96, 411);
+            this.btnNavi.Name = "btnNavi";
+            this.btnNavi.Size = new System.Drawing.Size(77, 23);
+            this.btnNavi.TabIndex = 3;
+            this.btnNavi.Text = "길찾기";
+            this.btnNavi.UseVisualStyleBackColor = true;
+            this.btnNavi.Click += new System.EventHandler(this.btnNavi_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(122, 34);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(51, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(12, 34);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(104, 21);
+            this.tbSearch.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.axMap1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dgvData);
+            this.splitContainer2.Size = new System.Drawing.Size(717, 623);
+            this.splitContainer2.SplitterDistance = 434;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // axMap1
+            // 
+            this.axMap1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMap1.Enabled = true;
+            this.axMap1.Location = new System.Drawing.Point(0, 0);
+            this.axMap1.Name = "axMap1";
+            this.axMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap1.OcxState")));
+            this.axMap1.Size = new System.Drawing.Size(717, 434);
+            this.axMap1.TabIndex = 0;
+            this.axMap1.ProjectionMismatch += new AxMapWinGIS._DMapEvents_ProjectionMismatchEventHandler(this.axMap1_ProjectionMismatch);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvData.Location = new System.Drawing.Point(0, 0);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowTemplate.Height = 23;
+            this.dgvData.Size = new System.Drawing.Size(717, 185);
+            this.dgvData.TabIndex = 0;
+            this.dgvData.DoubleClick += new System.EventHandler(this.dgvData_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 623);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "공간DB 길찾기 프로그램";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.RadioButton rbTo;
+        private System.Windows.Forms.RadioButton rbFrom;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbTravelTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbRouteLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbSearch;
+        private System.Windows.Forms.Button btnNavi;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView dgvData;
+        private AxMapWinGIS.AxMap axMap1;
+    }
+}
+
---
+++ Final/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AxMapWinGIS;
+using MapWinGIS;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace Final
+{
+    public partial class Form1 : Form
+    {
+        JArray items;
+        double f_x;
+        double f_y;
+        double t_x;
+        double t_y;
+        int layerHandle3;
+        double sum_length = 0;
+        double last_agg_cost = 0;
+        Utils u = new Utils();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 맵좌표계설정
+            axMap1.Projection = tkMapProjection.PROJECTION_GOOGLE_MERCATOR;
+            // 배경맵적용(타일맵)
+            axMap1.TileProvider = tkTileProvider.OpenStreetMap;
+            // 맵초기공간범위
+            axMap1.KnownExtents = tkKnownExtents.keSouth_Korea;
+            // 맵커서모드변경
+            axMap1.CursorMode = tkCursorMode.cmPan;
+            // SelectBoxFinal 이벤트 사용시 꼭!!
+            axMap1.SendSelectBoxFinal = true;
+
+            rbFrom.Checked = true;
+        }
+
+        private void axMap1_ProjectionMismatch(object sender, _DMapEvents_ProjectionMismatchEvent e)
+        {
+            e.reproject = tkMwBoolean.blnTrue;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lbSearch.Items.Clear();
+
+            string target = tbSearch.Text;
+  
+            string url = string.Format("http://api.vworld.kr/req/search?service=search&request=search&version=2.0&crs=EPSG:4326&size=1000&page=1&query={0}&type=place&format=json&errorformat=json&key=4DC3C0AD-CFCA-3277-8293-B13CADFB6A7A", target);
+
+            // Rest API
+            var client = new RestClient(url);           // RestClient : 요청(url)을 서비스제공자(서버)에게 전달하는 객체
+            client.Timeout = -1;                       // 요청 시간이 너무 길면 요청 끊어라
+            var request = new RestRequest(Method.GET); // 요청 처리 방식 : GET방식 : 데이터 얻기만
+            IRestResponse response = client.Execute(request); // Client 일해라
+
+       
+            var result = JObject.Parse(response.Content);
+            // JSON 배열로 결과 저장, JSON 배열 = [JObject1, JObject2, ...JObjectN]  JObject = Json
+            items = (JArray)result["response"]["result"]["items"];          
+
+            //// 리스트박스에 채우기
+            // 리스트박스에 아이템 추가하기 : lstSearch.Items.Add("item");           
+            foreach (var item in items) // var = JObject
+            {
+                lbSearch.Items.Add(item["title"]);
+            }
+        }
+
+        private void lbSearch_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            axMap1.RemoveAllLayers();
+
+            var item = items[lbSearch.SelectedIndex];
+
+            Shapefile sf = new Shapefile();
+            sf.CreateNew("", ShpfileType.SHP_POINT);
+
+            Shape shp = new Shape();
+            shp.Create(ShpfileType.SHP_POINT);
+            MapWinGIS.Point p = new MapWinGIS.Point();
+            p.x = Convert.ToDouble(item["point"]["x"]);
+            p.y = Convert.ToDouble(item["point"]["y"]);
+
+            int pointindex = shp.numPoints;
+            shp.InsertPoint(p, ref pointindex);
+
+            int shpindex = sf.NumShapes;
+            sf.EditInsertShape(shp, ref shpindex);
+
+            sf.GeoProjection.ImportFromEPSG(4326);
+
+            int center = axMap1.AddLayer(sf, true);
+            Shapefile sf2 = axMap1.get_Shapefile(center);
+            sf2.DefaultDrawingOptions.PointSize = 15;
+
+            axMap1.ZoomToShape(0, lbSearch.SelectedIndex);
+            axMap1.CurrentScale = 5000;
+
+            if (rbFrom.Checked == true)
+            {
+                tbFrom.Text = item["title"].ToString();
+                f_x = p.x;
+                f_y = p.y;
+            }
+            if (rbTo.Checked == true)
+            {
+                tbTo.Text = item["title"].ToString();
+                t_x = p.x;
+                t_y = p.y;
+            }
+        }
+        private void btnNavi_Click(object sender, EventArgs e)
+        {
+            axMap1.RemoveAllLayers();
+            dgvData.DataSource = null;
+
+            string connStr = "PG:host=localhost dbname=test user=postgres password=postgres port=5433";
+            string cmd;
+            cmd = string.Format("select node_id from moct_node where geom = (select ST_ClosestPoint(ST_Union(geom), ST_Transform(ST_SetSRID(ST_MakePoint({0}, {1}), 4326), 5186)) from moct_node)", f_x, f_y);
+            int layerHandle = axMap1.AddLayerFromDatabase(connStr, cmd, false);
+            Shapefile sf = axMap1.get_Shapefile(layerHandle);
+            int f_node_id = Convert.ToInt32(sf.get_CellValue(0, 0));
+
+            cmd = string.Format("select node_id from moct_node where geom = (select ST_ClosestPoint(ST_Union(geom), ST_Transform(ST_SetSRID(ST_MakePoint({0}, {1}), 4326), 5186)) from moct_node)", t_x, t_y);
+            int layerHandle2 = axMap1.AddLayerFromDatabase(connStr, cmd, false);
+            Shapefile sf2 = axMap1.get_Shapefile(layerHandle2);
+            int t_node_id = Convert.ToInt32(sf2.get_CellValue(0, 0));
+
+            cmd = string.Format("select seq, edge, agg_cost, road_name, ST_Length(geom) as length, speed, ST_Transform(geom, 4326) from moct_link as x join (select * from pgr_dijkstra('select CAST(link_id as bigint) as id, CAST(f_node as bigint) as source, CAST(t_node as bigint) as target, ST_Length(geom) / speed as cost from moct_link', {0}, {1})) as y on CAST(x.link_id as bigint) = y.edge order by seq", f_node_id, t_node_id);
+            layerHandle3 = axMap1.AddLayerFromDatabase(connStr, cmd, true);
+            Shapefile sf3 = axMap1.get_Shapefile(layerHandle3);
+            sf3.DefaultDrawingOptions.LineWidth = 7;
+            double min = Convert.ToDouble(sf3.Table.MinValue[5]);
+            double max = Convert.ToDouble(sf3.Table.MaxValue[5]);
+            sf3.Categories.AddRange(5, tkClassificationType.ctNaturalBreaks, 5, min, max);
+            sf3.Categories.get_Item(0).DrawingOptions.FillColor = u.ColorByName(tkMapColor.Green);
+            sf3.Categories.get_Item(1).DrawingOptions.FillColor = u.ColorByName(tkMapColor.GreenYellow);
+            sf3.Categories.get_Item(2).DrawingOptions.FillColor = u.ColorByName(tkMapColor.Yellow);
+            sf3.Categories.get_Item(3).DrawingOptions.FillColor = u.ColorByName(tkMapColor.Salmon);
+            sf3.Categories.get_Item(4).DrawingOptions.FillColor = u.ColorByName(tkMapColor.Red);
+            sf3.Categories.ApplyExpressions();
+
+            CreateTable();
+
+            tbRouteLength.Text = Convert.ToString(sum_length/1000);
+            tbTravelTime.Text = Convert.ToString(last_agg_cost);
+        }
+        private void CreateTable()
+        {
+            DataTable dt = new DataTable();
+            Shapefile sf = axMap1.get_Shapefile(layerHandle3);
+            
+            for (int i = 0; i < sf.NumFields; i++)
+            {
+                DataColumn dc = new DataColumn();
+                dc.ColumnName = sf.Table.Field[i].Name;
+                dt.Columns.Add(dc);
+            }
+       
+            for (int i = 0; i < sf.NumShapes; i++) // 행 개수
+            {
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < sf.NumFields; j++) // 컬럼개수
+                {
+                    if (sf.get_CellValue(j, i) != null) // null 경우 제외
+                    {
+                        string val = sf.get_CellValue(j, i).ToString();
+                        dr[j] = val;
+                    }
+                }
+                dt.Rows.Add(dr); // 테이블 로우 추가
+            }
+
+            // DGV의 데이터소스 할당
+            dgvData.DataSource = dt;
+          
+            foreach (DataRow item in dt.Rows)
+            {
+                double length = Convert.ToDouble(item["length"]);
+                sum_length += length;
+                last_agg_cost = Convert.ToDouble(item["agg_cost"]);
+            }
+        }
+
+        private void dgvData_DoubleClick(object sender, EventArgs e)
+        {
+            axMap1.ZoomToShape(0, dgvData.CurrentRow.Index);
+            axMap1.CurrentScale = 5000;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            lbSearch.Items.Clear();
+            dgvData.DataSource = null;
+            axMap1.Redraw();
+
+            tbFrom.Text = "";
+            tbTo.Text = "";
+            tbSearch.Text = "";
+            tbRouteLength.Text = "";
+            tbTravelTime.Text = "";
+
+            rbFrom.Checked = true;
+        }
+    }
+}
